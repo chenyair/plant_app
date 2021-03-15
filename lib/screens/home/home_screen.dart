2d27af86295edfde68495acff4d1a973161450f8
Yair Chen <yair.chen2000@gmail.com>
2021-03-15 23:00:40 +0200

    create scaffold for details screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../../widgets/my_bottom_navbar.dart';
-import 'body.dart';
+import '../../widgets/my_bottom_navbar.dart';
+import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key key}) : super(key: key);
